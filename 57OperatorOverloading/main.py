a9bd5d7b97b355d76639b5b08d0016a656401ffc
Vedant-GitLab <vedant_246023@saitm.ac.in>
2026-09-22 15:07:38 +0530

    Operator Overloading

--- 57OperatorOverloading/main.py
+++ 57OperatorOverloading/main.py
@@ -2,3 +2,24 @@
 
 #Why do we need operator overloading? : Operator overloading allows you to create more readable and intuitive code. For instance, consider a custom class that represents a point in 2D space. You could define a method called 'add' to add two points together, but using the + operator makes the code more concise and readable
 
+class Vector:
+    def __init__(self, i, j, k):
+        self.i = i
+        self.j = j
+        self.k = k
+
+    def __str__(self):
+        return f"{self.i}i + {self.j}j + {self.k}k"
+
+    def __add__(self, x):
+        # return f"{self.i + x.i}i + {self.j + x.j}j + {self.k + x.k}k"   #<class 'str'>
+        return Vector (self.i + x.i, self.j + x.j, self.k + x.k) #<class '__main__.Vector'>
+
+v1 = Vector(3, 5, 6)
+print(v1)
+
+v2 = Vector(1, 7, 9)
+print(v2)
+
+print(v1 + v2)
+print(type(v1 + v2))
